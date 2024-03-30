b5a2e66793843e1c9e7ec13f9400cc795f37262e
ZAQ00 <zubairahmedqazi@outlook.com>
2024-03-31 02:29:54 +0500

    Included window movements

--- clock.py
+++ clock.py
@@ -10,7 +10,13 @@ def changeTime():
 def closeWindow():
     window.destroy()
     
+def moveWindow(event):
+    x, y = event.x_root, event.y_root
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
 
+    if 0 < x < screen_width and 0 < y < screen_height:
+        window.geometry('+{0}+{1}'.format(x, y))
 
 window = tk.Tk()
 window.title('Digital Clock')
@@ -31,5 +37,6 @@ label.pack(padx=20, pady=20)
 
 changeTime()
 
+bar.bind('<B1-Motion>', moveWindow)
 
 window.mainloop()
